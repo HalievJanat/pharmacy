7e44f4a572ef1cfb442d828d2e8fa2fdbe5c6838
nikita <nikita>
2024-05-13 23:03:45 +0400

    Добавлена страница 3(изменил)

--- digital-department-application/src/app/page3/page3.component.ts
+++ digital-department-application/src/app/page3/page3.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-page3',
-  standalone: true,
-  imports: [],
+  selector: 'page3.component',
   templateUrl: './page3.component.html',
-  styleUrl: './page3.component.css'
+  styleUrls: ['./page3.component.css']
 })
 export class Page3Component {
 
