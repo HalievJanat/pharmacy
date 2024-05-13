b95bafe15ea76e1ce05901502826bf3334f11d6e
HalievJanat <152373700+HalievJanat@users.noreply.github.com>
2024-05-13 22:05:12 +0400

    Добавил проект

---
+++ digital-department-application/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { Page1Component } from './page1/page1.component';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: Page1Component }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Page1Component
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ digital-department-application/src/app/page1/page1.component.ts
@@ -0,0 +1,70 @@
+import { Component} from '@angular/core';
+import { NgModel } from '@angular/forms';
+
+export interface Pharmacy {
+  number: number;
+  count: number;
+  category: 'Гормоны';
+  name?: string;
+  dose?: string;
+  expirationDate?: Date;
+  existence: boolean;
+  structureArray?: string[];
+}
+@Component({
+  selector: 'page1.component',
+  templateUrl: './page1.component.html',
+  styleUrls: ['./page1.component.css']
+})
+export class Page1Component  {
+  structure: string = '';
+  structureSelected: string = '';
+  tasks: Pharmacy[] = [];
+  newTask: Pharmacy = {
+    number: 0,
+    count: 0,
+    category: 'Гормоны',
+    existence: false
+  };
+  filteredTasks: Pharmacy[] = [];
+  selectedTaskNumber: string = 'all';
+  selectedTaskNumberForEdit: number = 1;
+  selectedTask: Pharmacy | null = null;
+  addTask(titlein: NgModel, creatorin: NgModel) {
+    this.newTask.structureArray = this.structure.split(',');
+    this.newTask.number = this.tasks.length + 1;
+    this.tasks.push(this.newTask);
+    titlein.control.markAsUntouched();
+    creatorin.control.markAsUntouched();
+    this.resetForm();
+  }
+  resetForm() {
+    this.newTask = {
+      number: 0,
+      count: 0,
+      category: 'Гормоны',
+      existence: false
+    };
+  }
+  loadTaskForEditing() {
+    this.selectedTask = this.tasks[this.selectedTaskNumberForEdit-1];
+  }
+  updateTask() {
+    if (this.selectedTask != null)
+      this.selectedTask.structureArray = this.structureSelected.split(',');
+    this.selectedTask = null;
+  }
+  rejectNonNumeric(event: { key: string; preventDefault: () => void; }): void {
+    // Если введенное значение не является числом, запретим его ввод
+    if (event.key && !event.key.match(/^\d+$/)) {
+      event.preventDefault();
+    }
+  }
+  onDateInput(event: any) {
+    const input = event.target.value;
+    const formattedInput = input
+      .replace(/\D/g, '')
+      .replace(/(\d{2})(\d{2})(\d{0,4})/, '$1/$2/$3');
+    event.target.value = formattedInput;
+  }
+}
