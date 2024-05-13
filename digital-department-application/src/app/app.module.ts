bebdd1465cd59c66694b4000473b09a773dac49c
nikita <nikita>
2024-05-13 22:53:33 +0400

    Добавлена страница 3

--- digital-department-application/src/app/app.module.ts
+++ digital-department-application/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { Page1Component } from './page1/page1.component';
+import { Page3Component } from './page3/page3.component';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
@@ -13,7 +14,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component
+    Page1Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
